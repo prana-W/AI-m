969aa1c6558f2395ba8d08ce417bd94371852585
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-08-20 03:53:56 +0530

    Change Routing for the entire app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, ApiKeys, About, NotFound, Models } from './pages';
-import { Header, Footer } from './components';
 import ErrorBoundary from './components/ErrorBoundary';
+import Layout from './Layout.jsx';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "api-keys",
+        element: <ApiKeys />,
+      },
+      {
+        path: "models",
+        element: <Models />,
+      },
+        {
+            path: "about",
+            element: <About />,
+        },
+        {
+            path: "*",
+            element: <NotFound />,
+        },
+    ],
+  },
+]);
 
 function App() {
     return (
         <ErrorBoundary>
-            <Router>
-                {/* Full-height flex container */}
-                <div className="min-h-screen flex flex-col">
-                    <Header />
-                    {/* Main grows and lets pages like Home stretch */}
-                    <main className="flex-1 flex flex-col">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/api-keys" element={<ApiKeys />} />
-                            <Route path="/about" element={<About />} />
-                            <Route path="/models" element={<Models />} />
-                            <Route path="*" element={<NotFound />} />
-                        </Routes>
-                    </main>
-                </div>
-                <Footer />
-            </Router>
+            <RouterProvider router={router} />
         </ErrorBoundary>
     );
 }
---
+++ src/Layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "./components";
+
+function Layout() {
+    return (
+        <>
+            <div className="min-h-screen flex flex-col">
+                <Header />
+                <main className="flex-1 flex flex-col">
+                    <Outlet />
+                </main>
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Layout;
