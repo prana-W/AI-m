ede22fee0596b046adef4093259e08efe6c5f51a
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-08-20 14:48:18 +0530

    Revert changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import App from './App.jsx';
 import './index.css';
 import { Toaster } from '@/components/ui/sonner';
 import handleError from '@/utils/errorHandler';
-import { Analytics } from "@vercel/analytics/next"
 
 window.onerror = (msg, src, line, col, error) => {
     handleError(error || msg, 'Global Error');
